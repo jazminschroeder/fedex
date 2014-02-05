a37e7702a7a451ea460f4a584a5fbc11e09998af
Ramón Roche <mrpollo@gmail.com>
2014-02-04 18:04:46 -0800

    fix debug items

--- lib/fedex/request/shipment.rb
+++ lib/fedex/request/shipment.rb
@@ -22,8 +22,7 @@ module Fedex
       # The parsed Fedex response is available in #response_details
       # e.g. response_details[:completed_shipment_detail][:completed_package_details][:tracking_ids][:tracking_number]
       def process_request
-        xml = build_xml
-        api_response = self.class.post api_url, :body => xml
+        api_response = self.class.post api_url, :body => build_xml
         puts api_response if @debug
         response = parse_response(api_response)
         if success?(response)
