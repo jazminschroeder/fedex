51c4b291415fc5c5a626dcfa29b6bb0fc0e64d11
CJ <chirag7jain@gmail.com>
2014-10-17 13:47:24 +0530

    getting all waybills associated with tracking number

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,6 @@ require 'rspec'
 require 'fedex'
 require 'support/vcr'
 require 'support/credentials'
-require 'active_support/all'
 
 RSpec.configure do |c|
   c.filter_run_excluding :production unless fedex_production_credentials
