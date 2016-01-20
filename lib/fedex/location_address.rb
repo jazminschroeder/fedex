15e5535d9752623c08255fcdea4eac1d9687b884
Erik Johansson <erik@peacock.se>
2016-01-20 16:51:52 +0100

    removed name from location address

--- lib/fedex/location_address.rb
+++ lib/fedex/location_address.rb
@@ -4,7 +4,7 @@ module Fedex
 		attr_reader :street_lines, :city, :postal_code, :country_code, :residential, :name
 
 		def initialize(options = {})
-		   	@name = options[:address_ancillary_detail][:additional_descriptions].first
+	#	   	@name = options[:address_ancillary_detail][:additional_descriptions].first
 		    options[:address].tap do |address|
 		    	@street_lines	= address[:street_lines]
 		    	@city 			= address[:city]
