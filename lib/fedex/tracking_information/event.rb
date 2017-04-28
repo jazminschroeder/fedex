91749222e301c4a4ece531c6b249ec167c561019
Christine Lau <clau@stitchfix.com>
2017-04-28 16:21:15 -0700

    fixed typo for tracking_information, changed occured_at => occurred_at

--- lib/fedex/tracking_information/event.rb
+++ lib/fedex/tracking_information/event.rb
@@ -1,13 +1,13 @@
 module Fedex
   class TrackingInformation
     class Event
-      attr_reader :description, :type, :occured_at, :city, :state, :postal_code,
+      attr_reader :description, :type, :occurred_at, :city, :state, :postal_code,
                   :country, :residential, :exception_code, :exception_description
 
       def initialize(details = {})
         @description           = details[:event_description]
         @type                  = details[:event_type]
-        @occured_at            = Time.parse(details[:timestamp])
+        @occurred_at            = Time.parse(details[:timestamp])
         @city                  = details[:address][:city]
         @state                 = details[:address][:state_or_province_code]
         @postal_code           = details[:address][:postal_code]
