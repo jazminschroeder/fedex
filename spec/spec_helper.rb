92bd7d5f342a85ec75ab7033ef1ebe923a80dc21
Calvin Hughes <calvin@veeqo.com>
2019-07-19 12:26:37 +0100

    Require RUN_PRODUCTION_SPECS env variable before running production specs We'll only want to run these in specific cases using the credentials, so require this to be passed explicitly.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,7 @@ require 'support/vcr'
 require 'support/credentials'
 
 RSpec.configure do |config|
-  config.filter_run_excluding :production if fedex_production_credentials.empty?
+  config.filter_run_excluding :production unless ENV['RUN_PRODUCTION_SPECS']
 
   config.expect_with :rspec do |expect_config|
     expect_config.syntax = :expect
