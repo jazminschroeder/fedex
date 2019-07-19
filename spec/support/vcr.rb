db94e382393d1ce68c076de3c774c9f0855c2ad3
Calvin Hughes <calvin@veeqo.com>
2019-07-19 11:52:44 +0100

    Store VCR cassettes and filter sensitive FedEx credentials

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -1,8 +1,20 @@
 require 'vcr'
 
-VCR.configure do |c|
-  c.cassette_library_dir  = File.expand_path('../../vcr', __FILE__)
-  c.hook_into :webmock
+VCR.configure do |config|
+  config.cassette_library_dir  = File.expand_path('../../vcr', __FILE__)
+  config.hook_into :webmock
+
+  config.filter_sensitive_data('FEDEX_DEVELOPMENT_CREDENTIAL_KEY') { fedex_development_credentials[:key] }
+  config.filter_sensitive_data('FEDEX_DEVELOPMENT_CREDENTIAL_PASSWORD') { fedex_development_credentials[:password] }
+  config.filter_sensitive_data('FEDEX_DEVELOPMENT_CREDENTIAL_ACCOUNT_NUMBER') { fedex_development_credentials[:account_number] }
+  config.filter_sensitive_data('FEDEX_DEVELOPMENT_CREDENTIAL_METER') { fedex_development_credentials[:meter] }
+  config.filter_sensitive_data('FEDEX_DEVELOPMENT_CREDENTIAL_MODE') { fedex_development_credentials[:mode] }
+
+  config.filter_sensitive_data('FEDEX_PRODUCTION_CREDENTIAL_KEY') { fedex_production_credentials[:key] }
+  config.filter_sensitive_data('FEDEX_PRODUCTION_CREDENTIAL_PASSWORD') { fedex_production_credentials[:password] }
+  config.filter_sensitive_data('FEDEX_PRODUCTION_CREDENTIAL_ACCOUNT_NUMBER') { fedex_production_credentials[:account_number] }
+  config.filter_sensitive_data('FEDEX_PRODUCTION_CREDENTIAL_METER') { fedex_production_credentials[:meter] }
+  config.filter_sensitive_data('FEDEX_PRODUCTION_CREDENTIAL_MODE') { fedex_production_credentials[:mode] }
 end
 
 RSpec.configure do |c|
